a769dbf5c8026586e414b6af36f4cc6e9f661e32
Your Name <you@example.com>
2024-08-05 04:02:33 +0800

    update

--- chatbot.py
+++ chatbot.py
@@ -52,7 +52,7 @@ class BasicChatbot:
             chunk_overlap=0
         )
         model_name = "BAAI/bge-m3"
-        model_kwargs = {"device": "cpu"}
+        model_kwargs = {"device": "cuda"}
         encode_kwargs = {"normalize_embeddings": True}
         embeddings = HuggingFaceBgeEmbeddings(
             model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
